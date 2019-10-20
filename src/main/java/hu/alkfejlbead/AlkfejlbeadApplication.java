d40309e3205bb184bb79fa6d408dd25be989a10f
V. K <31962736+vukornel@users.noreply.github.com>
2019-10-20 21:24:04 +0200

    Szerkezeti szerkesztések

---
+++ src/main/java/hu/alkfejlbead/AlkfejlbeadApplication.java
@@ -0,0 +1,12 @@
+package hu.alkfejlbead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkfejlbeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlkfejlbeadApplication.class, args);
+    }
+}
